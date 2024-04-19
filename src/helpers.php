4dc54f72cf976e5d350f22d65991eb49a5a888f2
Radoslav Boydev <radoslav.boydev@gmail.com>
2024-04-19 20:31:13 +0300

    Fixed issue with the helper function declaration

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,13 @@
 
 use function Illuminate\Filesystem\join_paths;
 
-if (function_exists('evolver_path')) {
+if (!function_exists('evolver_path')) {
+    /**
+     * Returns the base Laravel Evolver package path.
+     *
+     * @param $path
+     * @return string
+     */
     function evolver_path($path): string
     {
         return join_paths(__DIR__, '..', $path);
