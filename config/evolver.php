8b2dbd1d817b0a6d32c255e4771b9c68b296b04f
Radoslav Boydev <radoslav.boydev@gmail.com>
2024-04-19 15:57:56 +0300

    Updated documentation

--- config/evolver.php
+++ config/evolver.php
@@ -7,6 +7,8 @@ return [
     | Install actions
     |--------------------------------------------------------------------------
     |
+    | In the 'Install' settings, users can specify a list of actions
+    | to be executed during the installation of a new application.
     |
     */
 
@@ -22,6 +24,8 @@ return [
     | Upgrade actions
     |--------------------------------------------------------------------------
     |
+    | In the 'Upgrade' settings, users can define actions to be performed
+    | whenever the application requires updates or modifications.
     |
     */
 
